7a0920774328527b03416c5be37c39ea68fb14e1
Unknown <marcomunozperez@uma.es>
2018-06-03 00:56:53 +0200

    Changed msa file read to biopython

--- AlignmentViewer/main.py
+++ AlignmentViewer/main.py
@@ -1,5 +1,7 @@
+from collections import OrderedDict
 from string import ascii_lowercase
 import plotly
+from Bio import SeqIO
 
 LETTERS = {letter: index for index, letter in enumerate(ascii_lowercase, start=1)}
 LETTERS['-'] = -1
@@ -15,11 +17,15 @@ def plot_alignment(numbered_sequences):
     y = list(numbered_sequences)
     z = list(numbered_sequences.values())
     x = list(range(len(z[0])))
-    trace = plotly.graph_objs.Heatmap(z=z, x=x, y=y)
+    trace = plotly.graph_objs.Heatmap(z=z,
+                                      x=x,
+                                      y=y,
+                                      showscale=False)
     layout = plotly.graph_objs.Layout(
         title='MSA Preview',
         xaxis=dict(ticks=''),
-        yaxis=dict(ticks='')
+        yaxis=dict(ticks=''),
+        showlegend=True
     )
     data = [trace]
     fig = plotly.graph_objs.Figure(data=data, layout=layout)
@@ -27,13 +33,26 @@ def plot_alignment(numbered_sequences):
 
 
 def process_fna(filepath):
-    with open(filepath, 'r') as fna:
-        sequence_dict = dict()
-        header = fna.readline()
-        while header != '':
-            sequence_dict[header] = alphabet_position(fna.readline().strip())
-            header = fna.readline()
+    fna = SeqIO.parse(filepath, "fasta")
+    sequence_dict = OrderedDict()
+    for sequence in fna:
+        sequence_dict[str(sequence.id)] = alphabet_position(sequence)
+
     return sequence_dict
 
 
-#plot_alignment(process_fna('VAR.BB11001.tsv'))
+def reduce_alignment_color(alignment_dict):
+    for sequence_position in range(len(alignment_dict[list(alignment_dict.keys())[0]])):
+        number = alignment_dict[list(alignment_dict.keys())[0]][sequence_position]
+        value = 0
+        for key in alignment_dict:
+            if alignment_dict[key][sequence_position] != number:
+                value = 1
+                break
+        for key in alignment_dict:
+            alignment_dict[key][sequence_position] = value
+
+    return alignment_dict
+
+
+plot_alignment(reduce_alignment_color(process_fna('filtered_haplotypes.afa')))
