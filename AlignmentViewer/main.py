22f2f6ecd7a4699cf2d0afe9a83c85a4f7e5baa5
Unknown <marcomunozperez@uma.es>
2018-06-05 13:35:55 +0200

    Improved alignment viewer

--- AlignmentViewer/main.py
+++ AlignmentViewer/main.py
@@ -1,9 +1,8 @@
 '''
 author: Marco Muñoz Perez
 email: marcomunozperez@uma.es
+github: @mmp1100000
 bitbucket: @mampz
-
-TODO CONVERT INTO CLASS.
 '''
 
 from collections import OrderedDict
@@ -12,20 +11,38 @@ import plotly
 from Bio import SeqIO
 import sys
 
+
 class ViewMSA:
-    def __init__(self, file_out):
+    """
+    This class generates a HTML heatmap that visualizes a MSA. Output file is
+    alignment_representation.html.
+
+    Usage: python3 /msa/file/path.afa
+    """
+
+    def __init__(self, file_in):
         self.LETTERS = {letter: index for index, letter in enumerate(ascii_lowercase, start=1)}
         self.LETTERS['-'] = -1
-        #self.plot_alignment(self.reduce_alignment_color(self.process_fna(file_out)))
-        self.plot_alignment(self.process_fna(file_out))
+        # self.plot_alignment(self.reduce_alignment_color(self.process_fna(file_out)))
+        self.plot_alignment(self.process_fna(file_in))
 
     def alphabet_position(self, text):
+        """
+        Converts a string (sequence to a list of numbers).
+        :param sequence:
+        :return: list of numbers
+        """
         text = text.lower()
         numbers = [self.LETTERS[character] for character in text if character in self.LETTERS]
         return numbers
 
     @staticmethod
     def plot_alignment(numbered_sequences):
+        """
+        Plots the dictionary as a heatmap in Plotly
+        :param numbered_sequences:
+        :return:
+        """
         y = list(numbered_sequences)
         z = list(numbered_sequences.values())
         x = list(range(len(z[0])))
@@ -44,6 +61,11 @@ class ViewMSA:
         plotly.offline.plot(fig, filename='alignment_representation.html')
 
     def process_fna(self, file_path):
+        """
+        Read the fasta file and store sequences to ordered dictionary
+        :param file_path:
+        :return: dictionary of sequence id:sequence as numbers
+        """
         fna = SeqIO.parse(file_path, "fasta")
         sequence_dict = OrderedDict()
         for sequence in fna:
@@ -53,6 +75,7 @@ class ViewMSA:
 
     @staticmethod
     def reduce_alignment_color(alignment_dict):
+        # Optional function as an alternative to the main plot representation
         for sequence_position in range(len(alignment_dict[list(alignment_dict.keys())[0]])):
             number = alignment_dict[list(alignment_dict.keys())[0]][sequence_position]
             value = 0
@@ -66,6 +89,5 @@ class ViewMSA:
         return alignment_dict
 
 
-ViewMSA(sys.argv[1])
-
-
+if __name__ == "__main__":
+    ViewMSA(sys.argv[1])
